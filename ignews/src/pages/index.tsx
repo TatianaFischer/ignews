1b85ec16bcf96b4ad4527397b3a501801df4e1a9
tatianafischer <tatiana.fischer@acad.pucrs.br>
2021-06-05 09:01:03 -0300

    [feat] consuming the Stripe API: price

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,10 +1,22 @@
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
 
+import { stripe } from '../services/stripe';
+
+import { GetServerSideProps } from 'next';
+
 import styles from './home.module.scss';
 
+interface HomeProps {
 
-export default function Home() {
+  product: {
+
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({product}: HomeProps) {
   return (
     <>
      <Head>
@@ -15,7 +27,7 @@ export default function Home() {
           <span> 👏 Hey, welcome</span>
           <h1>Newa about <br/>the <span>React</span> world.</h1>
           <p>Get access to all the publications <br/>
-          <span>for $9.90 month</span>
+          <span>for {product.amount} month</span>
           </p>
           <SubscribeButton/>
         </section>
@@ -27,3 +39,31 @@ export default function Home() {
     
   )
 }
+
+export const getServerSideProps: GetServerSideProps = async() => {
+//pode ser dessa forma ou como HTTP
+//retrieve é quando só quer um dado
+//no parâmetro usar o Id do preço/dado que tu quer, é la no site do stripe que se acha products/dados que quer/API ID
+//expand: para ter acesso a todas as informações do product, não somente ao preço, isso é uma forma de exemplificar quando ha mais de uma opção de preço, podendo buscar o titulo de cada preço e etc.
+
+//para ver quais os nomes dos dados que podemos buscar é só entrar na documentão do stripe: https://stripe.com/docs/api/prices/retrieve
+
+//preço na documentação é amoutn e ele vem em centavos, por isso a divisão por 100: amount: price.unit_amount / 100 (sempre salvar em centavos no banco de dados)
+  const price = await stripe.prices.retrieve('price_1IyxAgJhNHS6L7nvN11WrR3p', {
+    expand: ['product']
+  })
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100) ,
+  }
+  return { 
+    props: {
+      product,
+    }
+  }
+}
+
