1d6da946422acd9543a44edfa8aa606e25d555cf
tatianafischer <tatiana.fischer@acad.pucrs.br>
2021-06-05 09:07:34 -0300

    [feat] consuming the Stripe API: price id button

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -29,7 +29,7 @@ export default function Home({product}: HomeProps) {
           <p>Get access to all the publications <br/>
           <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton/>
+          <SubscribeButton priceId={product.priceId}/>
         </section>
 
         <img src="/images/avatar.svg" alt='Menina codando'/>;
@@ -53,13 +53,13 @@ export const getServerSideProps: GetServerSideProps = async() => {
     expand: ['product']
   })
 
-  const product = {
-    priceId: price.id,
-    amount: new Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency: 'USD',
-    }).format(price.unit_amount / 100) ,
-  }
+    const product = {
+      priceId: price.id,
+      amount: new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+      }).format(price.unit_amount / 100) ,
+    }
   return { 
     props: {
       product,
