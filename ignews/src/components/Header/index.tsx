c47d7bf5ef98b6fa59251b645e32acf8f2080311
tatianafischer <tatiana.fischer@acad.pucrs.br>
2022-10-31 21:06:21 -0300

    ActiveLink component color with cloneElement from react

--- ignews/src/components/Header/index.tsx
+++ ignews/src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
+import { ActiveLink } from '../ActiveLink';
 
 export function Header() {
   return (
@@ -8,14 +9,13 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="Logomarca ig.news" />
         <nav>
-          <Link href="/">
-            <a className={styles.active}>Home</a>
-          </Link>
-          <Link href="/posts" prefetch>
+          <ActiveLink href="/" activeClassName={styles.active}>
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink href="/posts" prefetch activeClassName={styles.active}>
             <a>Posts</a>
-          </Link>
+          </ActiveLink>
         </nav>
-
         <SignInButton />
       </div>
     </header>
