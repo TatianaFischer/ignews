59c2bd8229ef928d323b8b116cd11b10690a45d5
tatianafischer <tatiana.fischer@acad.pucrs.br>
2022-11-05 12:23:36 -0300

    Post html style

--- ignews/src/pages/posts/[slug].tsx
+++ ignews/src/pages/posts/[slug].tsx
@@ -1,19 +1,38 @@
 import { GetServerSideProps } from 'next';
 import { getSession } from 'next-auth/client';
+import Head from 'next/head';
 import { RichText } from 'prismic-dom';
 import { getPrismicClient } from '../../services/prismic';
-
+import styles from './post.module.scss';
 interface PostProps {
   post: {
     slug: string;
     title: string;
     content: string;
-    updated: string;
+    updatedAt: string;
   };
 }
 
 export default function Post({ post }: PostProps) {
-  return <h1>Teste</h1>;
+  return (
+    <>
+      <Head>
+        <title>{post.title} | Ignews</title>
+      </Head>
+      <main className={styles.container}>
+        <article className={styles.post}>
+          <h1>{post.title}</h1>
+          <time>{post.updatedAt}</time>
+          {/* quando estamos puxando um html, precisamos usar uma proprieade de dentro do 
+          elemento react que se chama dangerouslySetInnerHTML. Esse método é perigoso de ser usado, mas nesse caso, como estamos buscando uma informação do prismic e o prismic faz o tratamento para impedir qualquer perigo e script maliciooso, vamos usar ele */}
+          <div
+            className={styles.postContent}
+            dangerouslySetInnerHTML={{ __html: post.content }}
+          />
+        </article>
+      </main>
+    </>
+  );
 }
 
 export const getServerSideProps: GetServerSideProps = async ({
